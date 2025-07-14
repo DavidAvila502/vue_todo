ce3a6ac0dfa6abbe5766231cf58238a8e0447161
David Avila <davidavila6002@gmail.com>
2025-07-14 15:46:43 -0600

    Profile selector created

---
+++ src/components/ProfileSelector.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const props = defineProps<{ profileImage: string; profileName: string }>()
+</script>
+
+<template>
+  <div class="profile-selector-main-container">
+    <img class="profile-selector-image" :src="props.profileImage" :alt="profileName + '.png'" />
+
+    <p class="profile-name">{{ props.profileName }}</p>
+  </div>
+</template>
+
+<style scoped>
+.profile-selector-main-container {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  cursor: pointer;
+  max-width: 200px;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+}
+
+.profile-selector-image {
+  border-radius: 100%;
+  width: 120px;
+  height: 120px;
+}
+
+.profile-name {
+  text-align: center;
+  color: white;
+  font-family: 'Roboto Mono', monospace;
+  font-weight: bold;
+  text-overflow: ellipsis;
+}
+
+.profile-selector-main-container:hover .profile-name {
+  color: #39e58c;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,71 @@
+<script setup lang="ts">
+import ProfileSelector from '@/components/ProfileSelector.vue'
+import { useProfilesStore } from '@/stores/profiles'
+
+const { profiles } = useProfilesStore()
+</script>
+
 <template>
   <div class="home-main-container">
-    <p>Hello world</p>
+    <p class="main-title">Profiles</p>
+
+    <seciton class="section-profile-selector">
+      <p class="section-title">Select a profile</p>
+
+      <div class="profile-selector-container">
+        <profile-selector
+          v-for="(profileData, index) in profiles"
+          :key="index"
+          :profile-image="profileData.localImage!"
+          :profile-name="profileData.profileName!"
+        />
+      </div>
+    </seciton>
   </div>
 </template>
 
 <style scoped>
 .home-main-container {
   min-height: 100vh;
+  max-width: 1536px;
+  margin: 0 auto;
+  padding: 10px;
+  /* background-color: aliceblue; */
+}
+
+.main-title {
+  text-align: center;
+  color: #39e58c;
+  font-size: 30px;
+  font-family: 'Roboto Mono', monospace;
+  margin-top: 20px;
+}
+.section-profile-selector {
+  display: flex;
+  flex-direction: column;
+  max-width: 1280px;
+  width: 100%;
+  margin: 0 auto;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
+}
+
+.section-title {
+  color: white;
+  max-width: 1280px;
+  margin: 0 auto;
+  font-family: 'Roboto Mono', monospace;
+  font-size: 20px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+.profile-selector-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin-top: 40px;
 }
 </style>
